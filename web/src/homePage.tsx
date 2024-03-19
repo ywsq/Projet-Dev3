3651345d5e666c931dd5d8539ac43aa3ea2619ba
yassa <y.wasiq@students.ephec.be>
2024-03-19 16:11:21 +0100

    Add Unversioned Files

---
+++ web/src/homePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <body>
+            <section>
+                <p>Page of homePage</p>
+            </section>
+
+        </body>
+    );
+}
+
+export default HomePage;
