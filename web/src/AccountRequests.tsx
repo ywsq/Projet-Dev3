b881fb2e022adc7c75fb75620857d05a168f3b88
yassa <y.wasiq@students.ephec.be>
2024-03-31 17:00:10 +0200

    Add page's file

---
+++ web/src/AccountRequests.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AccountRequests.css';
+
+function AccountRequests() {
+    return (
+        <body>
+            <p>page requests</p>
+        </body>
+    );
+}
+
+export default AccountRequests;
