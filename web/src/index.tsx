c0b7bcef58db26bd93c8b6232db4e9f0bd771f21
yassa <y.wasiq@students.ephec.be>
2024-04-01 19:04:59 +0200

    Remove banniere from every page

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -24,7 +24,6 @@ const root = createRoot(container!);
 root.render(
     <React.StrictMode>
         <Router>
-            <Banniere />
             <body>
             <Routes>
                 <Route path="/" element={<HomePage />} />
