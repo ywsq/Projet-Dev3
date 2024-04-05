541bab03d98a9443fd3a4d26c1e83c123a12ae10
yassa <y.wasiq@students.ephec.be>
2024-04-05 12:10:11 +0200

    Add Banniere Partner

--- web/src/BannierePartner.tsx
+++ web/src/BannierePartner.tsx
@@ -1,71 +1,72 @@
-import React from 'react';
-import './Banniere.css';
-import Logo from './assets/logoStarNoBack.png'
-import burgerMenu from './assets/menu_FILL0_wght400_GRAD0_opsz24.svg'
-import expand from './assets/expand_more_FILL0_wght400_GRAD0_opsz24.svg'
-import { useNavigate } from 'react-router-dom';
-import {useState} from 'react';
+import React, { useState } from 'react';
+import Logo from './assets/logoStarNoBack-rmbg.png'
+import expand from "./assets/expand_more_FILL0_wght400_GRAD0_opsz24.svg";
+import burgerMenu from "./assets/menu_FILL0_wght400_GRAD0_opsz24.svg";
+import { useNavigate } from "react-router-dom";
+import Search from './assets/search_FILL0_wght400_GRAD0_opsz24.svg'
+import Cart from './assets/shopping_cart_FILL0_wght400_GRAD0_opsz24.svg'
+import Account from './assets/account_circle_FILL0_wght400_GRAD0_opsz24.svg'
 
-function Banniere() {
-    const navigate = useNavigate();
+function BannierePartner() {
     const [menuVisible, setMenuVisible] = useState(false);
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setMenuVisible(!menuVisible);
     };
 
-    const LoginPage = () => {
-        // This will navigate to second component
-        navigate('/Login');
-    };
-
     return (
-        <header className="bg-white">
-            <nav className="flex px-4 border-b items-center relative">
-                <div className="text-lg font-bold md:py-2 py-4">
-                    <img className="h-14 max-w-xs" src={Logo} alt="Logo"/>
+        <header>
+            <nav className="flex px-4 py-1 border-b justify-between items-center relative">
+                <div className="flex flex-col md:flex-row items-center flex-grow">
+                    <a href="#" className="xl:ml-20 lg:ml-14 md:ml-5">
+                        <img className="h-12 max-w-xs" src={Logo} alt="Logo"/>
+                    </a>
+                    <ul className={`items-center bg-white md:px-2 ml-5 md:flex md:space-x-2 ${menuVisible ? '' : 'hidden md:block'}`}>
+                        <li>
+                            <a href="#" className="flex md:inline-flex p-4 md:mt-2 items-center hover:bg-sky-400 hover:text-white rounded">MyProduct</a>
+                        </li>
+                        <li className="relative parent">
+                            <a href="/Catalog" className="flex justify-between md:inline-flex p-4 md:mt-2 items-center hover:bg-sky-400 hover:text-white space-x-2 rounded group">
+                                <span>Catalog</span>
+                                <img className="w-4 h-4 hidden md:block group-hover:invert" src={expand} alt="Expand icon"/>
+                            </a>
+                            <ul className="child transition duration-300 md:absolute top-full right-0 md:w-48 bg-white md:shadow-lg md:rounded-b ">
+                                <li>
+                                    <a href="#" className="flex p-4 hover:bg-sky-400 hover:text-white rounded">Devices</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="flex p-4 hover:bg-sky-400 hover:text-white rounded">Components</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="flex p-4 hover:bg-sky-400 hover:text-white rounded">Accessories</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" className="flex md:hidden md:inline-flex p-4 items-center hover:bg-sky-400 hover:text-white rounded">Cart</a>
+                        </li>
+                        <li>
+                            <a href="#" className="flex md:hidden md:inline-flex p-4 items-center hover:bg-sky-400 hover:text-white rounded">Account</a>
+                        </li>
+                    </ul>
+                    <input type="search" name="search" placeholder="Search" className="hidden md:block border-2 bg-white h-10 px-5 md:mt-2 w-full rounded-full text-sm focus:outline-sky-500 focus:border-white"/>
                 </div>
-                <ul className={`bg-white md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full left-0 right-0 ${menuVisible ? '' : 'hidden'}`}>
-                    <li>
-                        <a href="/Products" className="flex md:inline-flex p-4 items-center hover:bg-gray-200 rounded-xl">Products</a>
-                    </li>
-                    <li className="relative parent">
-                        <a href="/Service"
-                           className="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-200 space-x-2 rounded-xl">
-                            <span>Services</span>
-                            <img className={`w-4 h-4 ${menuVisible ? 'hidden' : ''}`} src={expand} alt="Expand icon"/>
-                        </a>
-                        <ul className="child transition duration-300 md:absolute top-full right-0 md:w-48 bg-white md:shadow-lg md:rounded-b ">
-                            <li>
-                                <a href="#" className="flex px-4 py-3 hover:bg-gray-200 rounded">Devices</a>
-                            </li>
-                            <li>
-                                <a href="#" className="flex px-4 py-3 hover:bg-gray-200 rounded">Components</a>
-                            </li>
-                            <li>
-                                <a href="#" className="flex px-4 py-3 hover:bg-gray-200 rounded">Accessories</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="/Store" className="flex md:inline-flex p-4 items-center hover:bg-gray-200 rounded-xl">Stores</a>
-                    </li>
-                    <li>
-                        <a href="/Partnership" className="flex md:inline-flex p-4 items-center hover:bg-gray-200 rounded-xl">Partnership</a>
-                    </li>
-                    <li>
-                        <a href="/Login" className={`flex md:inline-flex p-4 items-center hover:bg-orange-500 hover:text-white text-orange-500 ${menuVisible ? 'bg-white text-orange-400' : 'hover:bg-orange-500'} focus:ring-4 focus:outline-none focus:ring-orange-300 font-semibold rounded-xl text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition ease-in duration-200`}>
-                            Login
-                        </a>
-                    </li>
-                </ul>
+                <div className="hidden md:flex md:mt-2">
+                    <a href="#" className="ml-10 mr-4">
+                        <img className="h-7" src={Cart} alt="Cart icon"/>
+                    </a>
+                    <a href="#" className="xl:mr-24 lg:mr-14 md:mr-5">
+                        <img className="h-7" src={Account} alt="Account icon"/>
+                    </a>
+                </div>
+
                 <div className="ml-auto md:hidden text-gray-500 cursor-pointer" onClick={toggleMenu}>
-                    <img className="h-8 max-w-xs" src={burgerMenu} alt="menu icon"/>
+                    <img className="absolute top-4 right-4 h-8 mr-5 max-w-xs" src={burgerMenu} alt="Menu icon"/>
                 </div>
             </nav>
         </header>
     );
 }
 
-
-export default Banniere;
+export default BannierePartner;
