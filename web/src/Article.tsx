579f02eab97eb415b012b8289ca340c43c3e50b6
4lex42 <a.laroy@students.ephec.be>
2024-04-12 21:27:01 +0200

    Add Article File

---
+++ web/src/Article.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banniere from "./Banniere";
+
+function Article() {
+    return (
+        <div>
+            <Banniere />
+        <p>Page of Article</p>
+    </div>
+);
+}
+
+export default Article;
