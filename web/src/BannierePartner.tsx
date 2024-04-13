7a2ac6c46d9b93674ce56ae59b3dde4eca04ea67
yassa <y.wasiq@students.ephec.be>
2024-04-13 19:02:11 +0200

    Add style to Cart page

--- web/src/BannierePartner.tsx
+++ web/src/BannierePartner.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import Logo from './assets/logoStarNoBack-rmbg.png'
-import expand from "./assets/expand_more_FILL0_wght400_GRAD0_opsz24.svg";
 import burgerMenu from "./assets/menu_FILL0_wght400_GRAD0_opsz24.svg";
-import Cart from './assets/shopping_cart_FILL0_wght400_GRAD0_opsz24.svg'
 import { NavLink } from 'react-router-dom'
-import Account from './assets/account_circle_FILL0_wght400_GRAD0_opsz24.svg'
 
 function BannierePartner() {
     const [menuVisible, setMenuVisible] = useState(false);
@@ -52,7 +49,7 @@ function BannierePartner() {
                     <input type="search" name="search" placeholder="Search" className="hidden md:block border-2 bg-white h-10 px-5 md:mt-2 w-full rounded-full text-sm focus:outline-sky-500 focus:border-white"/>
                 </div>
                 <div className="hidden md:flex md:mt-2">
-                    <NavLink to="#" className="ml-10 mr-4">
+                    <NavLink to="/App" className="ml-10 mr-4">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
                              className="w-7 h-7 hover:scale-110 duration-200 fill-current text-black hover:text-sky-500"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>
                     </NavLink>
