a39ea26572040e707285244596124867846bb964
yassa <y.wasiq@students.ephec.be>
2024-04-14 22:06:54 +0200

    Add filter to remove buttons

--- web/src/AccountRequests.tsx
+++ web/src/AccountRequests.tsx
@@ -70,16 +70,26 @@ function AccountRequests() {
                     {data.map((item: any, index: number) => (
                         <div key={index} className="flex justify-between border-t hover:bg-gray-100">
                             <p className="p-3 px-5 bg-transparent">{item.Society_Name}<br />{item.Mail_Address}<br />{item.Phone_Number}<br />{item.ID_Country}</p>
-                            <div className="p-3 px-5 flex items-center">
-                                <button type="button"
-                                        onClick={() => handleClickAcceptRequest(item.ID_Client)}
-                                        className="h-1/2 mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline">Accept
-                                </button>
-                                <button type="button"
-                                        onClick={() => handleClickRefuseRequest(item.ID_Client)}
-                                        className="h-1/2 text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline">Refuse
-                                </button>
-                            </div>
+                            {filter === 'Waiting' ? (
+                                // Code HTML à afficher lorsque le filtre est "Waiting"
+                                <div className="p-3 px-5 flex items-center">
+                                    <button type="button"
+                                            onClick={() => handleClickAcceptRequest(item.ID_Client)}
+                                            className="h-1/2 mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline">Accept
+                                    </button>
+                                    <button type="button"
+                                            onClick={() => handleClickRefuseRequest(item.ID_Client)}
+                                            className="h-1/2 text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline">Refuse
+                                    </button>
+                                </div>
+                            ) : (
+                                // Code HTML à afficher lorsque le filtre n'est pas "Waiting"
+                                <div className="flex w-2/12 justify-center w-10">
+                                    <button title="Options">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z"/></svg>
+                                    </button>
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>
