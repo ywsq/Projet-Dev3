9fe4ee5bb1aeb827e65f67412295846ce8088e4f
yassa <y.wasiq@students.ephec.be>
2024-04-14 15:07:02 +0200

    Add pages to admin

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -18,6 +18,9 @@ import Test from './Test';
 import AccountRequests from './AccountRequests';
 import AdminAnalytics from "./AdminAnalytics";
 import AdminHome from "./AdminHome";
+import AdminCustomers from "./AdminCustomers";
+import AdminOrders from "./AdminOrders";
+import AdminProducts from "./AdminProducts";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -41,6 +44,9 @@ root.render(
                 <Route path="/AccountRequests" element={<AccountRequests />} />
                 <Route path="/AdminAnalytics" element={<AdminAnalytics />} />
                 <Route path="/AdminHome" element={<AdminHome />} />
+                <Route path="/AdminCustomers" element={<AdminCustomers />} />
+                <Route path="/AdminOrders" element={<AdminOrders />} />
+                <Route path="/AdminProducts" element={<AdminProducts />} />
             </Routes>
             </body>
         </Router>
