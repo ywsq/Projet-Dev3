0c8dd35f31617b6d21cf773a63a1d6dc22de8f0b
yassa <y.wasiq@students.ephec.be>
2024-04-18 17:03:20 +0200

    Modify footer

--- web/src/Footer.tsx
+++ web/src/Footer.tsx
@@ -4,16 +4,20 @@ import './Footer.css';
 function Footer() {
     return (
 
-        <footer className="footer">
-            <hr/>
-            <ul className="menu">
-                <li className="menu__item"><a className="menu__link" href="/">Home</a></li>
-                <li className="menu__item"><a className="menu__link" href="/Service">Services</a></li>
-                <li className="menu__item"><a className="menu__link" href="/Store">Store</a></li>
-                <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
-
-            </ul>
-            <p>&copy;2024 EPHEC-TI | All Rights Reserved</p>
+        <footer className="footer border-t">
+            <div className="flex w-full px-10 pt-10 pb-4 items-center justify-between">
+                <div className="space-x-10 text-gray-500">
+                    <a href="#" className="hover:text-black transition duration-500">About</a>
+                    <a href="#" className="hover:text-black transition duration-500">Returns Policy</a>
+                    <a href="#" className="hover:text-black transition duration-500">FAQs</a>
+                </div>
+                <div>
+                    <a href="#"
+                    className="border-b shadow-md py-3 px-5 rounded-xl bg-white text-gray-500 hover:text-gray-700 hover:shadow-sm transition duration-500">
+                        Contact</a>
+                </div>
+            </div>
+            <p className="select-none">&copy; Copyright 2024 Star Mobile All Rights Reserved.</p>
         </footer>
     );
 }
